4f7337a5bcd48d76ceebaf14e038c9d7bfef2a8c
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-12-11 12:42:03 +0300

    feat: implement animated progress bar and enhance element size handling
    - Added a new Progress component with animation support using motion for dynamic width based on progress percentage.
    - Updated use-element-size hook to ensure throttled size handling is executed correctly.
    - Integrated Progress component into the mobile header for displaying storage limits.
    - Minor formatting adjustments in use-squircle for improved readability.

--- src/shared/ui/squircle/use-squircle.tsx
+++ src/shared/ui/squircle/use-squircle.tsx
@@ -64,6 +64,7 @@ export const useSquircle = (props: UseSquircleProps) => {
     [style, path, disableSmoothing],
   );
 
+
   const wrapperStyle = useMemo(
     () => ({
       borderRadius: wrapperRadius,
--- src/widgets/header/ui/mobile.tsx
+++ src/widgets/header/ui/mobile.tsx
@@ -9,6 +9,7 @@ import { Squircle } from '@/src/shared/ui/squircle';
 import { ThemeSelector } from '@/src/shared/ui/theme-selector';
 import { User } from '@/src/shared/ui/user';
 import { Divider } from '@/src/shared/ui/flex/divider';
+import { Progress } from '@/src/shared/ui/progress';
 
 import { LogoutButton } from './logoutButton';
 
@@ -39,10 +40,17 @@ export const HeaderMobile = () => {
         <DrawerDescription hidden>Меню для мобильных устройств</DrawerDescription>
         <Flex col className='mt-8 gap-5 p-4'>
           <User {...user} />
-          <Flex align='center' className='gap-2 mt-2' justify='between'>
+          <Flex align='center' className='mt-2 gap-2' justify='between'>
             <p className='text-base font-medium'>Перейти в профиль</p>
             <PiCaretRightBold className='h-5 w-5 opacity-50' />
           </Flex>
+          <Divider />
+          <Flex col className='gap-3'>
+            <h4 className='text-base font-medium'>Лимиты хранилища</h4>
+            <Progress progress={50} />
+            <p className='text-foreground/50 text-sm'>2.02GB / 4GB (50%)</p>
+          </Flex>
+
           <Divider />
           <Flex className='items-center justify-between'>
             <p className='text-base font-medium'>Тема оформления</p>
